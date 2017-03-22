2e95d96d625a4c96d07c2d4503f56d79fdfdf301
andrew-rayco <andy@andycondon.com>
2017-03-22 13:21:06 +1300

    correct formatting in js file

--- events.js
+++ events.js
@@ -1,7 +1,7 @@
 // Don't change or delete this line! It waits until the DOM has loaded, then calls
 // the start function. More info:
 // https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
-document.addEventListener('DOMContentLoaded', start)
+document.addEventListener('DOMContentLoaded', start);
 
 function start () {
   // The first example is done for you. This will change the background colour of the first div
@@ -11,18 +11,18 @@ function start () {
   three();
   four();
   // Your turn! Create a new function called `two`, then call it from here.
-}
+};
 
 function one () {
   // First, we have to find the element:
-  var one = document.getElementById('one')
+  var one = document.getElementById('one');
 
   // Next, we add an event listener to it:
-  one.addEventListener('mouseenter', makeBlue)
+  one.addEventListener('mouseenter', makeBlue);
 
   // Finally, we add one to make the colour white again
-  one.addEventListener('mouseleave', makeWhite)
-}
+  one.addEventListener('mouseleave', makeWhite);
+};
 
 // CREATE FUNCTION two HERE
 function two() {
@@ -35,7 +35,7 @@ function three() {
   var three = document.getElementById('three');
   three.addEventListener('mouseenter', makeOrange);
   three.addEventListener('mouseleave', makeWhite);
-}
+};
 
 // CREATE FUNCTION four HERE
 function four() {
@@ -44,17 +44,17 @@ function four() {
   four.addEventListener('mousedown', makeBlue);
   four.addEventListener('mouseup', makeGreen);
   four.addEventListener('mouseleave', makeWhite);
-}
+};
 
 
 // Changes the background color of event's target
 function makeBlue (evt) {
-  evt.target.style.backgroundColor = 'blue'
-}
+  evt.target.style.backgroundColor = 'blue';
+};
 
 function makeWhite (evt) {
-  evt.target.style.backgroundColor = 'white'
-}
+  evt.target.style.backgroundColor = 'white';
+};
 
 function makeGreen (evt) {
   evt.target.style.backgroundColor = 'green';
@@ -62,4 +62,4 @@ function makeGreen (evt) {
 
 function makeOrange (evt) {
   evt.target.style.backgroundColor = 'orange';
-}
+};
